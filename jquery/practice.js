f968d9693113197b0cecae9346b5fceda928486d
Morari Cosmin <cosminmorari99@yahoo.com>
2023-01-04 21:56:56 +0200

    practice....

---
+++ jquery/practice.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $("a").click(function() {
+        $(this).addClass("active");
+        //$('a').removeClass("active");
+        //de aici sa vedem cum eliminam clasa anterioara
+    });
+});
